496f121bc04c2b87180b78ef6b45caf86e755439
brian23-eng <b.odhiambo.bo@gmail.com>
2019-08-16 22:40:51 +0300

    add action to the form

--- script.js
+++
@@ -1,7 +0,0 @@
-//declare variables
-function sub(){
-  var year = parseInt(document.getElementById("year").value);
-  var month = parseInt(document.getElementById("month").value);
-  var day = parseInt(document.getElementById("day").value);
-  var gender = document.getElementById("gender").value;
-}
