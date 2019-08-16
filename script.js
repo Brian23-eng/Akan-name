927fef47c9a4ca9284b30622c0b146de0d8df4df
brian23-eng <b.odhiambo.bo@gmail.com>
2019-08-16 19:39:47 +0300

    declare variables within the function sub()

---
+++ script.js
@@ -0,0 +1,7 @@
+//declare variables
+function sub(){
+  var year = parseInt(document.getElementById("year").value);
+  var month = parseInt(document.getElementById("month").value);
+  var day = parseInt(document.getElementById("day").value);
+  var gender = document.getElementById("gender").value;
+}
